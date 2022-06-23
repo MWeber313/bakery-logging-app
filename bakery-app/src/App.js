4366d367c8ced3f8654cf23b0b588b9de38d96ba
MWeberLambdaweb19 <mack.webb37@gmail.com>
2022-06-22 17:08:43 -0700

    Once more

--- bakery-app/src/App.js
+++ bakery-app/src/App.js
@@ -1,47 +1,21 @@
 import './App.css';
-import React, {useEffect, useState} from 'react';
-import {dateCalculator} from './HelperFunctions/dateCalculator.js';
+import React from 'react';
+import {dateCalculator, gregorianCalendarArray} from './HelperFunctions/dateCalculator.js';
+import isLeapYear from './HelperFunctions/isLeapYear';
 
 function App() {
   
   let today = new Date();
   
-  // const [sevenDays, setSevenDays] = useState(today.getUTCDate())
-  // const [sevenDaysMonth, setSevenDaysMonth] = useState(today.getUTCMonth()+1);
 
-  // const [fourteenDays, setFourteenDays] = useState(today.getUTCDate())
-  // const [fourteenDaysMonth, setFourteenDaysMonth] = useState(today.getUTCMonth()+1);
-
-  // const [twentyoneDays, setTwentyoneDays] = useState(today.getUTCDate())
-  // const [twentyoneDaysMonth, setTwentyoneDaysMonth] = useState(today.getUTCMonth()+1);
-
-  // const [twentyeightDays, setTwentyeightDays] = useState(today.getUTCDate())
-  // const [twentyeightDaysMonth, setTwentyeightDaysMonth] = useState(today.getUTCMonth()+1);
-
-  // useEffect(() => {
-  //   setSevenDays(today.getUTCDate(today.setUTCDate(today.getUTCDate()+7)));
-  //   setSevenDaysMonth(today.getUTCMonth(today.setUTCMonth(today.getUTCMonth()+1)));
-    
-  //   setFourteenDays(today.getUTCDate(today.setUTCDate(today.getUTCDate()-7+14)));
-  //   setFourteenDaysMonth(today.getUTCMonth(today.setUTCMonth(today.getUTCMonth())));
-    
-  //   setTwentyoneDays(today.getUTCDate(today.setUTCDate(today.getUTCDate()-14+21)));
-  //   setTwentyoneDaysMonth(today.getUTCMonth(today.setUTCMonth(today.getUTCMonth())));
-
-  //   setTwentyeightDays(today.getUTCDate(today.setUTCDate(today.getUTCDate()-21+28)));
-  //   setTwentyeightDaysMonth(today.getUTCMonth(today.setUTCMonth(today.getUTCMonth())));
-  // })
-  // console.log(today)
-  // console.log('7 days', sevenDays)
-
-  function isLeapYear (a) {
-    if (a%4 === 0) {
-      return true
-    }
-    else {
-      return false
-    }
-  }
+  // function isLeapYear (a) {
+  //   if (a%4 === 0) {
+  //     return true
+  //   }
+  //   else {
+  //     return false
+  //   }
+  // }
 
   // Get dates for 4 different dates:
   // Today + 7, +14, +21, +28
@@ -49,84 +23,8 @@ function App() {
   // Rolling over into next month and whether or not it is a leap year
 
   // Making an object to hold 4 different parameters of the modified date
-  let modifiedDate = {
-    datePlus7: 7,
-    datePlus14: 14,
-    datePlus21: 21,
-    datePlus28: 28
-  };
 
-  let currentMonth = today.getMonth();
 
-  // let dayMonth = {
-  //   Jan: 31,
-  //   Feb: 28,
-  //   Mar: 31,
-  //   Apr: 30,
-  //   May: 31,
-  //   Jun: 30,
-  //   Jul: 31,
-  //   Aug: 31,
-  //   Sept: 30,
-  //   Oct: 31,
-  //   Nov: 30,
-  //   Dec: 31
-  // }
-
-  // let lengthMonth = [
-  //   31,
-  //   28,
-  //   31,
-  //   30,
-  //   31,
-  //   30,
-  //   31,
-  //   31,
-  //   30,
-  //   31,
-  //   30,
-  //   31
-  // ]
-
-  // let monthArray = [
-  //   'Jan',
-  //   'Feb',
-  //   'Mar',
-  //   'Apr',
-  //   'May',
-  //   'June',
-  //   'July',
-  //   'Aug',
-  //   'Sept',
-  //   'Oct',
-  //   'Nov',
-  //   'Dec'
-  // ]
-
-  // for (let i in dayMonth) {
-  //   console.log(i === today.getMonth())
-  //   console.log(i, "=", today.getMonth())
-  // }
-
-let monthLength = [
-  {Month: "Jan", Length: 31},
-  {Month: "Feb", Length: 28},
-  {Month: "Mar", Length: 31},
-  {Month: "Apr", Length: 30},
-  {Month: "May", Length: 31},
-  {Month: "Jun", Length: 30},
-  {Month: "Jul", Length: 31},
-  {Month: "Aug", Length: 31},
-  {Month: "Sept", Length: 30},
-  {Month: "Oct", Length: 31},
-  {Month: "Nov", Length: 30},
-  {Month: "Dec", Length: 31}
-]
-
-// This function will map out the object and add the current day to the object's key/value pairs.
-function dateCalculator(dateToModify) {
-
-}
 
   return (
     <div className="App">
@@ -139,22 +37,10 @@ function dateCalculator(dateToModify) {
         </h5>
         <h5>Is a leap year: {isLeapYear(today.getFullYear()) ? "Yes" : "No"} </h5>
         <ul>
-          <li>7 Days: {today.getMonth() + 1} / {today.getUTCDate() + 7}</li>
-          <li>14 Days: {today.getMonth() + 1} / {today.getDate() + 14}</li>
-          <li>21 Days: {today.getMonth() + 1} / {today.getDate() + 21}</li>
-          <li>28 Days: {today.getMonth() + 1} / {today.getUTCDate() + 28}</li>
-        </ul>
-        {/* <ul>
-         <li>7 Days: {sevenDaysMonth} / {sevenDays}</li>
-          <li>14 Days: {fourteenDaysMonth} / {fourteenDays}</li>
-          <li>21 Days: {twentyoneDaysMonth} / {twentyoneDays}</li>
-          <li>28 Days: {twentyeightDaysMonth} / {twentyeightDays}</li>
-        </ul> */}
-        <ul>
-          <li>7 Days: {dateCalculator(7)}</li>
-          <li>14 Days: {}</li>
-          <li>21 Days: {}</li>
-          <li>28 Days: {}</li>
+          <li>7 Days: {dateCalculator(today, 7)}</li>
+          <li>14 Days: {dateCalculator(today, 14)}</li>
+          <li>21 Days: {dateCalculator(today, 21)}</li>
+          <li>28 Days: {dateCalculator(today, 28)}</li>
         </ul>
       </header>
     </div>
--- bakery-app/src/Components/sheetcalc.js
+++ bakery-app/src/Components/sheetcalc.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 class SheetCalculator extends React.Component (props) {
+    render() {
+        return 
+        <div>
+
+        </div>
+    }
 
 }
 
